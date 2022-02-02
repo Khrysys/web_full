13e0e8eb00e8875199085e9d4ad3b3cf8f202ea8
Comp01-sudo <70709374+Comp01-sudo@users.noreply.github.com>
2022-02-02 15:16:52 -0500

    Update system_monitor.py

--- system_monitor.py
+++ system_monitor.py
@@ -3,4 +3,5 @@ while True:
     os.system('cls')
     print("CPU: " + str(psutil.cpu_percent()))
     print("CPU Frequency: " + str(int(psutil.cpu_freq().current)))
+    print('RAM: ' + str(int(int(psutil.virtual_memory().total - psutil.virtual_memory().available))) + " B / " + str(int(psutil.virtual_memory().total())))
     time.sleep(1)
